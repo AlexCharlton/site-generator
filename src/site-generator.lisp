8b276eaf87f03e64aa9f43d65b313cecdf29a443
Alex Charlton <alex.n.charlton@gmail.com>
2013-09-03 17:34:27 -0400

    Increment release

--- src/site-generator.lisp
+++ src/site-generator.lisp
@@ -21,7 +21,7 @@
 
 (defconstant +version-major+ 0)
 (defconstant +version-minor+ 1)
-(defconstant +version-release+ 0)
+(defconstant +version-release+ 1)
 
 ;;;; ## Primary interface
 ;; Directories site-generator expects
